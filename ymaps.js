a3a1a1a9e69a2fb927aeb7b95448aec918dd9e39
oasisoasisoasis <41289250+oasisoasisoasis@users.noreply.github.com>
2018-07-16 18:22:48 +0300

    Update ymaps.js

--- ymaps.js
+++ ymaps.js
@@ -2,10 +2,6 @@ ymaps.ready(init);
 
 function init() {
     var myMap = new ymaps.Map("map", {
-            center: [55.76, 37.64],
-            zoom: 10
-        }, {
-            searchControlProvider: 'yandex#search'
         }),
 
     // Создаем геообъект с типом геометрии "Точка".
